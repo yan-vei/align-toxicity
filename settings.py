9c7df8e6def6d107dddad17973137e06815219b1
Yana Veitsman <yana.veitsman@yandex.kz>
2024-10-07 15:50:54 +0200

    add sweeps

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ from decouple import config
 class Settings(BaseSettings):
     """
     Class holding all the necessary config and env settings
-    for the experiments.
+    for the experiments.yaml.
     """
 
     WANDB_API_KEY: str = config("WANDB_API_KEY", cast=str)
